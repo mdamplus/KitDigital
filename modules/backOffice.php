c1ba1aa31342336862e9b9bca8195ccaf415b6cd
MDAM zZ <76702014+mdamplus@users.noreply.github.com>
2023-04-20 10:20:59 +0200

    Update backOffice.php
    backoffice Styles updated

--- modules/backOffice.php
+++ modules/backOffice.php
@@ -12,7 +12,6 @@ function z_wave_login_js() {
 }
 add_action( 'login_enqueue_scripts', 'z_wave_login_js' );
 
-
 //esta función cambia la url.
 function z_login_logo_url() {
     return 'https://informaticasa.es/servicios/kit-digital/?ztrack=login';
@@ -26,19 +25,38 @@ add_filter( 'login_headertitle', 'z_login_logo_url_title' );
 
 
 function custom_admin_css() {
+  
   echo '<style>
-    #wpadminbar {background: rgba(0,157,184,1); color:#fff;}
-    #adminmenu, #adminmenu .wp-submenu, #adminmenuback, #adminmenuwrap {background: rgba(0,157,184,0.9); color:#fff;}
-    #adminmenu .wp-has-current-submenu .wp-submenu .wp-submenu-head, #adminmenu .wp-menu-arrow, #adminmenu .wp-menu-arrow div, #adminmenu li.current a.menu-top, #adminmenu li.wp-has-current-submenu a.wp-has-current-submenu {background: rgba(248,177,51,1);}
-    wpadminbar:hover, #adminmenu li.menu-top:hover, #adminmenu li.opensub>a.menu-top, #adminmenu li>a.menu-top:focus{color:#fff; background:rgba(0,92,102,1);}
-    #adminmenu .wp-submenu a, #collapse-button, #adminmenu div.wp-menu-image {color:rgba(255,255,255,0.9)}
-    #adminmenu .wp-submenu a, #collapse-button, #adminmenu div.wp-menu-name{font-size:15px;}
-#adminmenu limenu-top:hover, #adminmenu li.opensub>a.menu-top, #adminmenu li>a.menu-top: focus {color:rgba(255,255,255,0.9);background-color:red;}
-#wpadminbar .quicklinks .menupop ul li a, #wpadminbar .quicklinks .menupop ul li a strong, #wpadminbar .quicklinks .menupop.hover ul li a, #wpadminbar.nojs .quicklinks .menupop:hover ul li a 
-#wpadminbar:not(.mobile) .ab-top-menu>li:hover>.ab-item {background:rgba(0,92,102,1) !important; color:#fff !important;}
-#wpadminbar #wp-admin-bar-supportlink .ab-item ab-icon:before { content: "\f484; top: 3px;}
+        #wpadminbar {background: rgba(0,157,184,1); color:#fff;}
+        #wpadminbar:hover, .ab-item:hover{color:#fff;}
 
-  </style>';
+
+#adminmenu .wp-has-current-submenu ul>li>a, .folded #adminmenu li.menu-top .wp-submenu>li>a:hover {
+background:rgba(0,157,184,1);
+color:white;
+}
+wpadminbar:hover #wp-admin-bar-wp-logo>.ab-item:hover .ab-icon:before {
+    content: "\f120";
+    top: 2px;
+    color: white;
+}
+
+	#wpadminbar .menupop .ab-sub-wrapper, #wpadminbar .shortlink-input, #wp-admin-bar-my-account:hover{background: rgba(0,157,184,1);}
+  	 #adminmenu, #adminmenu .wp-submenu, #adminmenuback, #adminmenuwrap,  .ab-top-menu { background: rgba(0,157,184,0.9); color:#fff;}
+        #adminmenu:hover, #adminmenuback:hover, #adminmenuwrap:hover{color:#fff;} 
+        #adminmenu .wp-has-current-submenu .wp-submenu .wp-submenu-head, #adminmenu .wp-menu-arrow, #adminmenu .wp-menu-arrow div, #adminmenu li.current a.menu-top, #adminmenu li.wp-has-current-submenu a.wp-has-current-submenu {background: rgba(248,177,51,1);}
+        wpadminbar:hover, #adminmenu li.menu-top:hover, #adminmenu li.opensub>a.menu-top, #adminmenu li>a.menu-top:focus{color:#fff; background:rgba(0,92,102,1);}
+        #adminmenu .wp-submenu a, #collapse-button, #adminmenu div.wp-menu-image {color:rgba(255,255,255,0.9);}
+        #adminmenu .wp-submenu a, #collapse-button, #adminmenu div.wp-menu-name{font-size:15px;}
+        #adminmenu limenu-top:hover, #adminmenu li.opensub>a.menu-top, #adminmenu li>a.menu-top: focus {color:rgba(255,255,255,0.9); background-color:red;}
+        #wpadminbar .quicklinks .menupop ul li a, #wpadminbar .quicklinks .menupop ul li a strong, #wpadminbar .quicklinks .menupop.hover ul li a, #wpadminbar.nojs .quicklinks .menupop:hover ul li a 
+        #wpadminbar:not(.mobile) .ab-top-menu>li:hover>.ab-item { color:#fff !important; }
+#adminmenu .wp-submenu a:focus, #adminmenu .wp-submenu a:hover, #adminmenu a:hover, #adminmenu li.menu-top>a:focus {
+    color: #b3e5f6;
+}
+
+        #wpadminbar #wp-admin-bar-supportlink .ab-item ab-icon:before { content: "\f484"; top: 3px;}
+    </style>';
 }
 add_action('admin_head', 'custom_admin_css');
 
